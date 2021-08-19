0a714a584a8fc084b84917866d29e50e4076c9a5
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-19 11:03:42 +0700

    first commit

---
+++ bin/async_await_example.dart
@@ -0,0 +1,40 @@
+import 'package:async_await_example/async_await_example.dart'
+    as async_await_example;
+
+void main(List<String> arguments) async {
+  var p = Person();
+
+  // Syncronous
+
+  // print('job1');
+  // print('job2');
+  // p.getData();
+  // print('job3');
+
+  // Asyncronous
+
+  print('job1');
+  print('job2');
+
+  // ignore: unawaited_futures
+  p.getDataAsync().then((_) {
+    print('job 3 : ' + p.name);
+  });
+
+  print('job4');
+}
+
+class Person {
+  late String name = 'default name';
+
+  void getData() {
+    name = 'Yusril';
+    print('Compleated!');
+  }
+
+  Future<void> getDataAsync() async {
+    await Future.delayed(Duration(seconds: 3));
+    name = 'Yusril';
+    print('Compleated!');
+  }
+}
